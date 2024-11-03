e1bb3ea5fea8dbc1223ca78b6cc17a1fb4dd3f8f
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2024-11-03 01:11:33 -0400

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# openbb-financialdatasets-backend
+# OpenBB with Financial Datasets backend
+
+1. Install all the required libraries
+
+```bash
+poetry install
+```
+
+2. Use your API Key
+
+Update your .env file with the API key obtained from your [Financial Datasets API Key dashboard](https://www.financialdatasets.ai/).
+
+3. Run the custom backend
+
+```bash
+uvicorn main:app --reload
+```
---
+++ main.py
@@ -0,0 +1,68 @@
+import json
+from pathlib import Path
+import os
+import requests
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+origins = [
+    "https://pro.openbb.co",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+FINANCIAL_DATASETS_API_KEY = os.getenv("FINANCIAL_DATASETS_API_KEY")
+
+
+@app.get("/")
+def read_root():
+    return {"Info": "Financial Datasets to integrate with OpenBB"}
+
+
+@app.get("/widgets.json")
+def get_widgets():
+    """Widgets configuration file for OpenBB"""
+    return JSONResponse(
+        content=json.load((Path(__file__).parent.resolve() / "widgets.json").open())
+    )
+
+
+@app.get("/income")
+def get_income(ticker: str, period: str, limit: int):
+    """Get income statement"""
+    # add your API key to the headers
+    headers = {
+        "X-API-KEY": FINANCIAL_DATASETS_API_KEY
+    }
+    # create the URL
+    url = (
+        f'https://api.financialdatasets.ai/financials/income-statements'
+        f'?ticker={ticker}'
+        f'&period={period}'
+        f'&limit={limit}'
+    )
+
+    print(f"Debug - Constructed URL: {url}")
+
+    # make API request
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        # parse income_statements from the response
+        income_statements = response.json().get('income_statements')
+
+        return income_statements
+
+    print(f"Request error {response.status_code}: {response.text}")
+    return JSONResponse(
+        content={"error": response.text}, status_code=response.status_code
+    )
